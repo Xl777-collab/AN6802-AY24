052e57af3e495b17b3bdf19366be69b50c65d0df
Xl777-collab <lihengxl777@gmail.com>
2025-03-07 12:44:07 +0000

    2

--- app.py
+++ app.py
@@ -6,5 +6,22 @@ app = Flask(__name__)
 def index():
     return(render_template("index.html"))
 
+@app.route("/main",methods=["GET","POST"])
+def main():
+    user_name = request.form.get("q")
+    return(render_template("main.html"))
+
+@app.route("/ethical_test",methods=["GET","POST"])
+def ethical_test():
+    return(render_template("ethical_test.html"))
+
+@app.route("/test_result",methods=["GET","POST"])
+def test_result():
+    answer = request.form.get("answer")
+    if answer=="true":
+        return(render_template("fail.html"))
+    elif answer=="false":
+        return(render_template("pass.html"))
+
 if __name__ == "__main__":
     app.run()
